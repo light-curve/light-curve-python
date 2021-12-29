52168f2c132716c55caf1fc904760449ae10521e
Konstantin Malanchev <hombit@gmail.com>
2021-12-29 12:27:24 -0600

    Don't use isort for the top-level __init__.py

--- light-curve/light_curve/__init__.py
+++ light-curve/light_curve/__init__.py
@@ -1,7 +1,13 @@
+# Prevent isort to sort imports in this file
+# isort: skip_file
+
+# Import all Python features
+from .light_curve_py import *
+
 # Hide Python features with Rust equivalents
 from .light_curve_ext import *
-from .light_curve_ext import __version__
 
 # Hide Rust Extractor with universal Python Extractor
-from .light_curve_py import *
 from .light_curve_py import Extractor
+
+from .light_curve_ext import __version__
