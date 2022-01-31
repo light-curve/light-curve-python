32ccb30e8e895887235bfbeb3203cd07bb9ed702
Konstantin Malanchev <hombit@gmail.com>
2022-01-31 11:02:16 -0600

    Initial HTML docs via pdoc3

--- light-curve/light_curve/__init__.py
+++ light-curve/light_curve/__init__.py
@@ -11,3 +11,17 @@ from .light_curve_ext import *
 from .light_curve_py import Extractor
 
 from .light_curve_ext import __version__
+from .light_curve_py.features._base import BaseFeature
+
+
+__all__ = {"_FeatureEvaluator", "BaseFeature", "DmDt", "Extractor"}
+__all__.update(c.__name__ for c in _FeatureEvaluator.__subclasses__() if c.__name__ in globals())
+__all__.update(c.__name__ for c in BaseFeature.__subclasses__() if c.__name__ in globals())
+__all__ = sorted(__all__)
+
+
+__pdoc__ = {c.__name__: c.__pdoc__ for c in _FeatureEvaluator.__subclasses__()}
+__pdoc__["_FeatureEvaluator"] = True
+__pdoc__["light_curve_py"] = False
+__pdoc__["light_curve_ext"] = False
+__pdoc__["light_curve"] = False
--- light-curve/light_curve/light_curve_ext.py
+++ light-curve/light_curve/light_curve_ext.py
@@ -1,1 +1,4 @@
 from .light_curve import *
+from .light_curve import _FeatureEvaluator
+
+__all__ = ["DmDt", "_FeatureEvaluator"] + [c.__name__ for c in _FeatureEvaluator.__subclasses__()]
