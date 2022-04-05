598ec7d976caf36e9c46d0aa1dcd293561f40ad5
Konstantin Malanchev <hombit@gmail.com>
2022-04-05 08:56:37 -0500

    pyo3 -> 0.16, rust-numpy -> 0.16

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,7 +13,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Changed
 
-—
+- **Braking:** drop Python 3.6 support
+- `rust-numpy` 0.15.1 -> 0.16.2
+- `py03` 0.15.1 -> 0.16.3
 
 ### Deprecated
 
--- light-curve/src/dmdt.rs
+++ light-curve/src/dmdt.rs
@@ -10,7 +10,6 @@ use enumflags2::{bitflags, BitFlags};
 use light_curve_dmdt as lcdmdt;
 use ndarray::IntoNdProducer;
 use numpy::{Element, IntoPyArray, PyArray1, ToPyArray};
-use pyo3::class::iter::PyIterProtocol;
 use pyo3::prelude::*;
 use pyo3::types::PyTuple;
 use rand::SeedableRng;
@@ -529,8 +528,8 @@ macro_rules! dmdt_batches {
             dmdt_batches: Arc<$generic>,
         }
 
-        #[pyproto]
-        impl PyIterProtocol for $name_batches {
+        #[pymethods]
+        impl $name_batches {
             fn __iter__(slf: PyRef<Self>) -> PyResult<Py<$name_iter>> {
                 let iter = $name_iter::new(slf.dmdt_batches.clone());
                 Py::new(slf.py(), iter)
@@ -613,8 +612,8 @@ macro_rules! dmdt_batches {
             }
         }
 
-        #[pyproto]
-        impl PyIterProtocol for $name_iter {
+        #[pymethods]
+        impl $name_iter {
             fn __iter__(slf: PyRef<Self>) -> PyRef<Self> {
                 slf
             }
