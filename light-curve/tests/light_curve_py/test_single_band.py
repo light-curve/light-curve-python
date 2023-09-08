795db54027d0311e0a6fb081761781b136c3a89a
Konstantin Malanchev <hombit@gmail.com>
2023-09-08 08:53:48 -0400

    Tests for multiband

--- light-curve/tests/light_curve_py/test_single_band.py
+++ light-curve/tests/light_curve_py/test_single_band.py
@@ -65,11 +65,29 @@ def test_init_bands_more_input_bands():
     sigma = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
     bands = ["g"] * 6
     feature = LinearFit(bands=["g", "r"])
-    # "r" band is virtually existing, but having zero observations,
-    # so it should be filled with the fill_value
+    # "r" band is virtually existing, but having zero observations
+    with pytest.raises(ValueError):
+        _values = feature(t, m, sigma, bands, fill_value=None)
+    # ... so it should be filled with the fill_value
     values = feature(t, m, sigma, bands, fill_value=np.nan)
     assert 2 == feature.n_bands
     assert values.size == feature.size
     assert values.size == feature.size_single_band * feature.n_bands
     assert np.all(np.isfinite(values[: feature.size_single_band]))
     assert np.all(np.isnan(values[feature.size_single_band :]))
+
+
+def test_init_bands_different_input_bands():
+    t = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
+    m = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
+    sigma = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+    bands = ["g"] * 6
+    feature = LinearFit(bands=["r"])
+    # "r" band is virtually existing, but having zero observations
+    with pytest.raises(ValueError):
+        _values = feature(t, m, sigma, bands, fill_value=None)
+    # ... so it should be filled with the fill_value
+    values = feature(t, m, sigma, bands, fill_value=-999.0)
+    assert 1 == feature.n_bands
+    assert values.size == feature.size == feature.size_single_band
+    np.testing.assert_array_equal(values, -999.0)
