bca668ab1665843fa720a85d657af102b1628970
Timofon <timofei.galushkin@gmail.com>
2024-02-25 11:29:55 +0300

    docstring and new tests for RoMS added

--- light-curve/light_curve/light_curve_py/features/roms.py
+++ light-curve/light_curve/light_curve_py/features/roms.py
@@ -4,6 +4,19 @@ from ._base import BaseSingleBandFeature
 
 
 class Roms(BaseSingleBandFeature):
+    r"""Robust median statistic
+
+    RoMS = $\[ (N-1)^{-1} \sum_{i=1}^{N} \frac{|m_i - median(m_i)|}{\sigma_i} \]$
+    For not variable data it should be less then one.   
+    If RoMS greater or equal one, it is indicate about potential variability in the data.
+
+    - Depends on: **magnitude**, **errors**
+    - Minimum number of observations: **2**
+    - Number of features: **1**
+
+    Enoch, Brown, Burgasser 2003. [DOI:10.1086/376598](https://www.doi.org/10.1086/376598)
+    """
+
     def _eval_single_band(self, t, m, sigma=None):
         n = len(m)
         median = np.median(m)
--- light-curve/tests/light_curve_py/features/test_roms.py
+++ light-curve/tests/light_curve_py/features/test_roms.py
@@ -8,8 +8,8 @@ def test_roms_1():
     feature = Roms()
     n = 100
     t = np.arange(n)
-    m = np.ones(n)
-    sigma = np.ones(n)
+    m = np.ones_like(n)
+    sigma = np.ones_like(n)
     actual = feature(t, m, sigma)
     desired = 0.0
     assert_allclose(actual, desired)
@@ -18,10 +18,75 @@ def test_roms_1():
 def test_roms_2():
     feature = Roms()
     n = 100
-    t = np.arange(n)
-    x = np.linspace(0, 10, n)
-    m = 10 * np.sin(x)
-    sigma = np.ones(n)
+    t = np.linspace(0, 2*np.pi, n)
+    m = 2 * np.sin(t)
+    sigma = np.ones_like(n)
     actual = feature(t, m, sigma)
-    desired = sum(abs(m - np.median(m))) / (n - 1)
-    assert_allclose(actual, desired)
+    desired = 1.162
+    assert_allclose(actual, desired, rtol=3/np.sqrt(n))
+
+def test_roms_3():
+    rng = np.random.default_rng(0)
+    n = 100
+    t = np.linspace(0, 1, n)
+    m = rng.normal(0, 1, n)
+    sigma = np.ones_like(t)
+    feature = Roms()
+    actual = feature(t, m, sigma)
+    desired = 0.797
+    assert_allclose(actual, desired, rtol=3/np.sqrt(n))
+
+def test_roms_4():
+    rng = np.random.default_rng(0)
+    n = 10000
+    t = np.linspace(0, 1, n)
+    m = rng.normal(0, 1, n)
+    sigma = np.ones_like(t)
+    feature = Roms()
+    actual = feature(t, m, sigma)
+    desired = 0.797
+    assert_allclose(actual, desired, rtol=3/np.sqrt(n))
+
+def test_roms_5():
+    rng = np.random.default_rng(0)
+    n = 100
+    t = np.linspace(0, 1, n)
+    m = rng.exponential(2, n)
+    sigma = np.ones_like(t)
+    feature = Roms()
+    actual = feature(t, m, sigma)
+    desired = 1.386
+    assert_allclose(actual, desired, rtol=3/np.sqrt(n))
+
+def test_roms_6():
+    rng = np.random.default_rng(0)
+    n = 10000
+    t = np.linspace(0, 1, n)
+    m = rng.exponential(2, n)
+    sigma = np.ones_like(t)
+    feature = Roms()
+    actual = feature(t, m, sigma)
+    desired = 1.386
+    assert_allclose(actual, desired, rtol=3/np.sqrt(n))
+
+def test_roms_7():
+    rng = np.random.default_rng(0)
+    n = 100
+    t = np.linspace(0, 1, n)
+    m = rng.gamma(2, 1, n)
+    sigma = np.ones_like(t)
+    feature = Roms()
+    actual = feature(t, m, sigma)
+    desired = 1.052
+    assert_allclose(actual, desired, rtol=3/np.sqrt(n))
+
+def test_roms_8():
+    rng = np.random.default_rng(0)
+    n = 10000
+    t = np.linspace(0, 1, n)
+    m = rng.gamma(2, 1, n)
+    sigma = np.ones_like(t)
+    feature = Roms()
+    actual = feature(t, m, sigma)
+    desired = 1.052
+    assert_allclose(actual, desired, rtol=3/np.sqrt(n))
