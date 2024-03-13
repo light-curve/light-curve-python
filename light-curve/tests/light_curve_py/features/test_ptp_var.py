af05f59997d36965ef40b611575f4f018bd367e9
Timofon <timofei.galushkin@gmail.com>
2024-03-13 13:04:45 +0300

    docstring and test update for peak-to-peak variation

--- light-curve/tests/light_curve_py/features/test_ptp_var.py
+++ light-curve/tests/light_curve_py/features/test_ptp_var.py
@@ -4,71 +4,18 @@ from numpy.testing import assert_allclose
 from light_curve.light_curve_py import PeakToPeakVar
 
 
-def test_ptpvar_const_data():
+def test_ptpvar():
     feature = PeakToPeakVar()
+    rng = np.random.default_rng(0)
     n = 100
+    m = 10000
+    lmd = 200
     t = np.arange(n)
-    m = np.ones_like(t)
-    sigma = 0.1 * np.ones_like(t)
-    actual = feature(t, m, sigma)
-    desired = -0.1
-    assert_allclose(actual, desired)
-
-
-def test_ptpvar_periodic_data():
-    feature = PeakToPeakVar()
-    n = 100
-    t = np.linspace(0, np.pi, n)
-    m = np.sin(t)
-    sigma = 0.1 * np.ones_like(t)
-    actual = feature(t, m, sigma)
-    desired = 0.8
-    assert_allclose(actual, desired, rtol=3 / np.sqrt(n))
-
-
-def test_ptpvar_norm_data_1():
-    rng = np.random.default_rng(0)
-    n = 100
-    t = np.linspace(0, 1, n)
-    m = np.abs(rng.normal(0, 1, n))
-    sigma = 0.2 * np.ones_like(t)
-    feature = PeakToPeakVar()
-    actual = feature(t, m, sigma)
-    desired = 1
-    assert_allclose(actual, desired, rtol=3 / np.sqrt(n))
-
-
-def test_ptpvar_norm_data_2():
-    rng = np.random.default_rng(0)
-    n = 10000
-    t = np.linspace(0, 1, n)
-    m = np.abs(rng.normal(0, 1, n))
-    sigma = 0.2 * np.ones_like(t)
-    feature = PeakToPeakVar()
-    actual = feature(t, m, sigma)
-    desired = 1
-    assert_allclose(actual, desired, rtol=3 / np.sqrt(n))
-
-
-def test_ptpvar_expon_data_1():
-    rng = np.random.default_rng(0)
-    n = 100
-    t = np.linspace(0, 1, n)
-    m = rng.exponential(2, n)
-    sigma = np.ones_like(t)
-    feature = PeakToPeakVar()
-    actual = feature(t, m, sigma)
-    desired = 1
-    assert_allclose(actual, desired, rtol=3 / np.sqrt(n))
-
-
-def test_ptpvar_expon_data_2():
-    rng = np.random.default_rng(0)
-    n = 10000
-    t = np.linspace(0, 1, n)
-    m = rng.exponential(2, n)
-    sigma = np.ones_like(t)
-    feature = PeakToPeakVar()
-    actual = feature(t, m, sigma)
-    desired = 1
-    assert_allclose(actual, desired, rtol=3 / np.sqrt(n))
+    flux_list = np.random.poisson(lmd, (m, n))
+    ptp_list = [PeakToPeakVar(t, flux_list[i], np.sqrt(flux_list[i])) for i in range(m)]
+    flux = rng.poisson(lmd, n)
+    sigma = np.sqrt(flux)
+    actual = feature(t, flux, sigma)
+    desired = (np.quantile(ptp_list, 0.025) + np.quantile(ptp_list, 0.975)) / 2
+    atol = desired - np.quantile(ptp_list, 0.025)
+    assert_allclose(actual, desired, atol=atol)
