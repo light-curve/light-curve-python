f74b3e0e3edd5eb822901c8122c7cb410df27c93
Konstantin Malanchev <hombit@gmail.com>
2025-04-30 16:37:12 -0400

    Cast to a common dtype
    # Conflicts:
    #	light-curve/src/np_array.rs

--- light-curve/src/np_array.rs
+++ light-curve/src/np_array.rs
@@ -1,7 +1,11 @@
 use crate::errors::{Exception, Res};
 
+use itertools::{izip, Either};
 use numpy::prelude::*;
-use numpy::{Element, PyArray1, PyReadonlyArray1, PyUntypedArray, PyUntypedArrayMethods};
+use numpy::{
+    AllowTypeChange, PyArray1, PyArrayLike1, PyReadonlyArray1, PyUntypedArray,
+    PyUntypedArrayMethods,
+};
 use pyo3::prelude::*;
 
 pub(crate) type Arr<'a, T> = PyReadonlyArray1<'a, T>;
@@ -22,7 +26,7 @@ impl DType for f64 {
     }
 }
 
-pub(crate) fn unknown_type_exception(name: &str, obj: Bound<PyAny>) -> Exception {
+pub(crate) fn unknown_type_exception(name: &str, obj: &Bound<PyAny>) -> Exception {
     let message = if let Ok(arr) = obj.downcast::<PyUntypedArray>() {
         let ndim = arr.ndim();
         if ndim != 1 {
@@ -46,59 +50,165 @@ pub(crate) fn unknown_type_exception(name: &str, obj: Bound<PyAny>) -> Exception
     Exception::TypeError(message)
 }
 
-pub(crate) fn extract_matched_array<'py, T>(
-    y_name: &'static str,
-    y: Bound<'py, PyAny>,
-    x_name: &'static str,
-    x: &Arr<'py, T>,
-    check_size: bool,
-) -> Res<Arr<'py, T>>
-where
-    T: Element + DType + 'py,
-{
-    if let Ok(y) = y.downcast::<PyArray1<T>>() {
-        let y = y.readonly();
-        if check_size {
-            if y.len() == x.len() {
-                Ok(y)
-            } else {
-                Err(Exception::ValueError(format!(
-                    "Mismatched lengths: '{}': {}, '{}': {}",
-                    x_name,
-                    x.len(),
-                    y_name,
-                    y.len(),
-                )))
-            }
+fn cast_fail_reason(
+    idx: usize,
+    names: &'static [&'static str],
+    objects: &[&Bound<PyAny>],
+    good_f32_arrays: &[PyReadonlyArray1<f32>],
+    good_f64_arrays: &[PyReadonlyArray1<f64>],
+) -> Exception {
+    let first_name = names.first().expect("Empty names slice");
+    let fist_obj = objects.first().expect("Empty objects slice");
+
+    // If the very first argument downcast
+    if idx == 0 {
+        return unknown_type_exception(first_name, fist_obj);
+    }
+
+    let (first_arr, first_dtype_name) = if good_f32_arrays.is_empty() {
+        (
+            good_f64_arrays
+                .first()
+                .expect("Empty good_f64_arrays slice")
+                .as_untyped(),
+            f64::dtype_name(),
+        )
+    } else {
+        (
+            good_f32_arrays
+                .first()
+                .expect("Empty good_f32_arrays slice")
+                .as_untyped(),
+            f32::dtype_name(),
+        )
+    };
+
+    let fail_name = names.get(idx).expect("idx is out of bounds of names slice");
+    let fail_obj = objects
+        .get(idx)
+        .expect("idx is out of bounds of names slice");
+
+    let error_message = if let Ok(fail_arr) = fail_obj.downcast::<PyUntypedArray>() {
+        if fail_arr.ndim() != 1 {
+            format!(
+                "'{}' is a {}-d array, only 1-d arrays are supported.",
+                fail_name,
+                fail_arr.ndim()
+            )
         } else {
-            Ok(y)
+            let first_arr_dtype_str = match first_arr.dtype().str() {
+                Ok(s) => s,
+                Err(err) => return err.into(),
+            };
+            let fail_arr_dtype_str = match fail_arr.dtype().str() {
+                Ok(s) => s,
+                Err(err) => return err.into(),
+            };
+            format!(
+                "Mismatched dtypes: '{first_name}': {first_arr_dtype_str}, '{fail_name}': {fail_arr_dtype_str}",
+            )
         }
     } else {
-        let error_message = if let Ok(y_arr) = y.downcast::<PyUntypedArray>() {
-            if y_arr.ndim() != 1 {
-                format!(
-                    "'{}' is a {}-d array, only 1-d arrays are supported.",
-                    y_name,
-                    y_arr.ndim()
-                )
-            } else {
-                format!(
-                    "Mismatched dtypes: '{}': {}, '{}': {}",
-                    x_name,
-                    x.dtype().str()?,
-                    y_name,
-                    y_arr.dtype().str()?
-                )
-            }
-        } else {
-            format!(
-                "'{y_name}' must be a numpy array of the same shape and dtype as '{x_name}', '{x_name}' has type 'np.ndarray[{x_dtype}]', '{y_name}' has type '{y_type}')",
-                y_type = y.get_type().name()?,
-                x_dtype = T::dtype_name()
-            )
+        let fail_obj_type_name = match fail_obj.get_type().name() {
+            Ok(s) => s,
+            Err(err) => return err.into(),
         };
-        Err(Exception::TypeError(error_message))
+        format!(
+            "'{fail_name}' must be a numpy array of the same shape and dtype as '{first_name}', '{first_name}' has type 'np.ndarray[{first_dtype_name}]', '{fail_name}' has type '{fail_obj_type_name}')",
+        )
+    };
+    Exception::TypeError(error_message)
+}
+
+pub(crate) enum GenericVecOfArrays<'py> {
+    F32(Vec<PyReadonlyArray1<'py, f32>>),
+    F64(Vec<PyReadonlyArray1<'py, f64>>),
+}
+
+impl<'py> GenericVecOfArrays<'py> {
+    fn first_len(&self) -> Option<usize> {
+        match self {
+            Self::F32(v) => v.first().map(|arr| arr.len()),
+            Self::F64(v) => v.first().map(|arr| arr.len()),
+        }
     }
+
+    fn iter_len(&'py self) -> impl Iterator<Item = usize> + 'py {
+        match self {
+            Self::F32(v) => Either::Left(v.iter().map(|arr| arr.len())),
+            Self::F64(v) => Either::Right(v.iter().map(|arr| arr.len())),
+        }
+    }
+}
+
+fn try_downcast_objects_to_f32_arrays<'py>(
+    objects: &[&Bound<'py, PyAny>],
+) -> Vec<PyReadonlyArray1<'py, f32>> {
+    objects
+        .iter()
+        .map(|&obj| obj.downcast::<PyArray1<f32>>())
+        .take_while(|result| result.is_ok())
+        // It is safe to unwrap, because we have just checked that it is Ok
+        .map(|result| result.unwrap().readonly())
+        .collect()
+}
+
+fn try_downcast_to_f64_array<'py>(obj: &Bound<'py, PyAny>) -> Option<PyReadonlyArray1<'py, f64>> {
+    if let Ok(py_array) = obj.downcast::<PyArray1<f64>>() {
+        Some(py_array.readonly())
+    } else if let Ok(as_array) = PyArrayLike1::<f64, AllowTypeChange>::extract_bound(obj) {
+        Some(as_array.readonly())
+    } else {
+        None
+    }
+}
+
+fn downcast_objects<'py>(
+    names: &'static [&'static str],
+    objects: &[&Bound<'py, PyAny>],
+) -> Res<GenericVecOfArrays<'py>> {
+    let f32_arrays = try_downcast_objects_to_f32_arrays(objects);
+    if f32_arrays.len() == names.len() {
+        Ok(GenericVecOfArrays::F32(f32_arrays))
+    } else {
+        let mut result = Vec::with_capacity(names.len());
+        for f32_arr in f32_arrays.iter() {
+            let f64_arr = f32_arr.cast::<f64>(false)?;
+            result.push(f64_arr.readonly());
+        }
+        for (idx, &obj) in objects.iter().enumerate().skip(result.len()) {
+            let py_ro_array = try_downcast_to_f64_array(obj)
+                .ok_or_else(|| cast_fail_reason(idx, names, objects, &f32_arrays, &result))?;
+            result.push(py_ro_array);
+        }
+        Ok(GenericVecOfArrays::F64(result))
+    }
+}
+
+pub(crate) fn downcast_and_validate<'py>(
+    names: &'static [&'static str],
+    objects: &[&Bound<'py, PyAny>],
+    check_size: &[bool],
+) -> Res<GenericVecOfArrays<'py>> {
+    assert_eq!(names.len(), objects.len());
+
+    let arrays = downcast_objects(names, objects)?;
+    let first_array_len = match arrays.first_len() {
+        Some(len) => len,
+        None => return Ok(arrays),
+    };
+    // We checked that 1) names size matches objects size, 2) objects is not empty
+    let first_name = names[0];
+
+    for (&name, length, &check) in izip!(&names[1..], arrays.iter_len().skip(1), check_size) {
+        if check && length != first_array_len {
+            return Err(Exception::ValueError(format!(
+                "Mismatched lengths: '{}': {}, '{}': {}",
+                first_name, first_array_len, name, length,
+            )));
+        }
+    }
+    Ok(arrays)
 }
 
 macro_rules! _distinguish_eq_symbol {
@@ -111,34 +221,35 @@ macro_rules! _distinguish_eq_symbol {
 }
 
 macro_rules! dtype_dispatch {
-    ($func: tt ($first_arg:expr $(,$eq:tt $arg:expr)* $(,)?)) => {
+    // @call variants are for the internal usage only
+    (@call $func:expr, $arrays:ident, $_arg1:expr,) => {{
+        let func = $func;
+        func($arrays[0].clone())
+    }};
+    (@call $func:expr, $arrays:ident, $_arg1:expr, $_arg2:expr,) => {{
+        let func = $func;
+        func($arrays[0].clone(), $arrays[1].clone())
+    }};
+    (@call $func:expr, $arrays:ident, $_arg1:expr, $_arg2:expr, $_arg3:expr,) => {{
+        let func = $func;
+        func($arrays[0].clone(), $arrays[1].clone(), $arrays[2].clone())
+    }};
+    ($func:tt ($first_arg:expr $(,$eq:tt $arg:expr)* $(,)?)) => {
         dtype_dispatch!($func, $func, $first_arg $(,$eq $arg)*)
     };
-    ($f32:expr, $f64:expr, $first_arg:expr $(,)?) => {{
-        if let Ok(x32) = $first_arg.downcast::<numpy::PyArray1<f32>>() {
-            let x32 = x32.readonly();
-            let f32 = $f32;
-            f32(x32)
-        } else if let Ok(x64) = $first_arg.downcast::<numpy::PyArray1<f64>>() {
-            let x64 = x64.readonly();
-            let f64 = $f64;
-            f64(x64)
-        } else {
-            Err(crate::np_array::unknown_type_exception(stringify!($first_arg), $first_arg.clone()))
-        }
-    }};
-    ($f32:expr, $f64:expr, $first_arg:expr $(,$eq:tt $arg:expr)+ $(,)?) => {{
-        let x_name = stringify!($first_arg);
-        if let Ok(x32) = $first_arg.downcast::<numpy::PyArray1<f32>>() {
-            let x32 = x32.readonly();
-            let f32 = $f32;
-            f32(x32.clone(), $(crate::np_array::extract_matched_array(stringify!($arg), $arg, x_name, &x32, _distinguish_eq_symbol!($eq))?,)*)
-        } else if let Ok(x64) = $first_arg.downcast::<numpy::PyArray1<f64>>() {
-            let x64 = x64.readonly();
-            let f64 = $f64;
-            f64(x64.clone(), $(crate::np_array::extract_matched_array(stringify!($arg), $arg, x_name, &x64, _distinguish_eq_symbol!($eq))?,)*)
-        } else {
-            Err(crate::np_array::unknown_type_exception(stringify!($first_arg), $first_arg.clone()))
+    ($f32:expr, $f64:expr, $first_arg:expr $(,$eq:tt $arg:expr)* $(,)?) => {{
+        let names = &[stringify!($first_arg), $(stringify!($arg), )*];
+        let objects = &[&$first_arg, $(&$arg, )*];
+        let check_size = &[ $(_distinguish_eq_symbol!($eq), )* ];
+
+        let generic_arrays = crate::np_array::downcast_and_validate(names, objects, check_size)?;
+        match generic_arrays {
+            crate::np_array::GenericVecOfArrays::F32(arrays) => {
+                dtype_dispatch!(@call $f32, arrays, $first_arg, $($arg,)*)
+            }
+            crate::np_array::GenericVecOfArrays::F64(arrays) => {
+                dtype_dispatch!(@call $f64, arrays, $first_arg, $($arg,)*)
+            }
         }
     }};
 }
