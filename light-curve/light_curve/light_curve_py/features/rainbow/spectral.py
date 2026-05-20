ec7ef35a13b5631b845522ace405eb71f2eeb0e7
erusseil <79919110+erusseil@users.noreply.github.com>
2026-05-20 17:00:22 +0200

    Merge pull request #705 from erusseil/main
    UV-extincted BB for Rainbow

---
+++ light-curve/light_curve/light_curve_py/features/rainbow/spectral.py
@@ -0,0 +1,123 @@
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Dict, List, Union
+
+import numpy as np
+
+__all__ = [
+    "spectral_terms",
+    "BaseSpectralTerm",
+    "PlanckSpectralTerm",
+    "BlanketedPlanckSpectralTerm",
+]
+
+# CODATA 2018
+planck_constant = 6.62607004e-27  # erg s
+speed_of_light = 2.99792458e10  # cm/s
+boltzman_constant = 1.380649e-16  # erg/K
+
+
+@dataclass()
+class BaseSpectralTerm:
+    """Spectral term for Rainbow"""
+
+    @staticmethod
+    @abstractmethod
+    def parameter_names() -> List[str]:
+        return NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def parameter_scalings() -> List[Union[str, None]]:
+        return NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def value(wave_cm, T, *params):
+        return NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def initial_guesses(t, m, sigma, band) -> Dict[str, float]:
+        return NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def limits(t, m, sigma, band) -> Dict[str, float]:
+        return NotImplementedError
+
+
+@dataclass()
+class PlanckSpectralTerm(BaseSpectralTerm):
+    """Standard blackbody spectrum"""
+
+    @staticmethod
+    def parameter_names():
+        return []
+
+    @staticmethod
+    def parameter_scalings():
+        return []
+
+    @staticmethod
+    def value(wave_cm, T, *params):
+        nu = speed_of_light / wave_cm
+
+        return (
+            (2 * planck_constant / speed_of_light**2) * nu**3 / np.expm1(planck_constant * nu / (boltzman_constant * T))
+        )
+
+    @staticmethod
+    def initial_guesses(t, m, sigma, band):
+        return {}
+
+    @staticmethod
+    def limits(t, m, sigma, band):
+        return {}
+
+
+@dataclass()
+class BlanketedPlanckSpectralTerm(BaseSpectralTerm):
+    """Blackbody spectrum with exponential blanketing"""
+
+    @staticmethod
+    def parameter_names():
+        return ["log_intensity", "lambda_scale"]
+
+    @staticmethod
+    def parameter_scalings():
+        return [None, None]
+
+    @staticmethod
+    def value(wave_cm, T, log_intensity, lambda_scale):
+        base = PlanckSpectralTerm.value(wave_cm, T)
+
+        # lambda_scale is expressed in Angstrom
+        lambda_scale_cm = lambda_scale * 1e-8
+
+        tau = 10**log_intensity * np.exp(-wave_cm / lambda_scale_cm)
+
+        return base * np.exp(-tau)
+
+    @staticmethod
+    def initial_guesses(t, m, sigma, band):
+        return {
+            "log_intensity": 1.5,
+            "lambda_scale": 100.0,
+        }
+
+    @staticmethod
+    def limits(t, m, sigma, band):
+        # lambda_scale and log_intensity are degenerate. Putting either to ~0 leads to a classical BB.
+        # We prevent log_intensity to go to 0, because the blanketing is more physically realistic
+        # at low lambda_scale high log_intensity rather than high lambda_scale low log_intensity.
+        return {
+            "log_intensity": (1.5, 6.0),
+            "lambda_scale": (100.0, 2000.0),
+        }
+
+
+spectral_terms = {
+    "planck": PlanckSpectralTerm,
+    "blanketed": BlanketedPlanckSpectralTerm,
+}
